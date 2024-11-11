78e5a5bce7709de8e57bff31e94b448d3e485031
Olibirds69 <baladadphilip@gmail.com>
2024-11-11 20:54:11 +0800

    Dice Roller v2 with Layout

---
+++ Diceroller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+        val rollButton: Button = findViewById(R.id.rollButton)
+
+        rollButton.setOnClickListener {
+            rollDice(diceImage)
+        }
+    }
+
+    private fun rollDice(diceImage: ImageView) {
+
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
